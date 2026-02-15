2dc7b444c112b084b1bab1268e95fef8a887f2c4
hlaymiabid <hlaymiabid@gmail.com>
2026-02-15 18:24:20 +0000

    firebase version not ready

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,7 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/postcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss, require('autoprefixer')],
-    },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    server: {
-      deps: {
-        inline: ['@csstools/css-calc'],
-      },
-    },
-  },
 })
